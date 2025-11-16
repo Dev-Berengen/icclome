ddd1e8836daa376ffdbd49e564b370864c382cc6
Dev-Berengen <dev.berengen@gmail.com>
2025-11-16 22:59:34 +0000

    0.009

---
+++ centrale.js
@@ -0,0 +1,304 @@
+export const data = [
+  {
+    id: 1,
+    img: "./assets/105.jpg",
+    nom: "NOKIA 105 | DOUBLE SIM",
+    stock: "Disponible",
+    prix: "6.000 Fcfa",
+    // Description de 20 mots Max
+    description:"",
+    liens: "https://wa.me/p/8074613042593108/22890381883",
+  },
+  {
+    id: 2,
+    img: "./assets/106.jpg",
+    nom: "NOKIA 106 | DOUBLE SIM",
+    stock: "jean",
+    prix: "6.000 Fcfa",
+    // Description de 20 mots Max
+    description:"",
+    liens: "https://wa.me/p/26634452412867674/22890381883",
+  },
+   {
+    id: 3,
+    img: "./assets/3310.jpg",
+    nom: "NOKIA 3310 | DOUBLE SIM",
+    prix: "7.000 Fcfa",
+    // Description de 20 mots Max
+    description:"",
+    liens: "",
+  },
+   {
+    id: 4,
+    img: "./assets/Bose.jpg",
+    nom: "CASQUE | BOSE",
+    prix: "9.000 Fcfa",
+    // Description de 20 mots Max
+    description:"",
+    liens: "",
+  },
+     {
+    id: 5,
+    img: "./assets/Bose-700.jpg",
+    nom: "CASQUE | BOSE 700",
+    prix: "9.000 Fcfa",
+    // Description de 20 mots Max
+    description:"",
+    liens: "",
+  },
+  {
+    id: 6,
+    img: "./assets/K30.jpg",
+    nom: "ECOUTEUR JBL | K30",
+    prix: "4.500 Fcfa",
+    // Description de 20 mots Max
+    description:"",
+    liens: "",
+  },
+  {
+    id: 7,
+    img: "./assets/JBL-K73.jpg",
+    nom: "ECOUTEUR JBL | K73",
+    prix: "5.000 Fcfa",
+    // Description de 20 mots Max
+    description:"",
+    liens: "",
+  },
+   {
+    id: 8,
+    img: "./assets/Souris-Chargable.jpg",
+    nom: "SOURIS CHARGABLE",
+    prix: "3.500 Fcfa",
+    // Description de 20 mots Max
+    description:"",
+    liens: "",
+  },
+   {
+    id: 9,
+    img: "./assets/Support-Ordinateur.jpg",
+    nom: "SUPPORT ORDINATEUR",
+    prix: "4.000 Fcfa",
+    // Description de 20 mots Max
+    description:"",
+    liens: "",
+  },
+  {
+    id: 10,
+    img: "./assets/USB-67W.jpg",
+    nom: "CHARGEUR 67W | USB-C",
+    prix: "3.500 Fcfa",
+    // Description de 20 mots Max
+    description:"",
+    liens: "",
+  },
+
+  {
+    id: 20,
+    img: "./assets/USB-67W-2.jpg",
+    nom: "CHARGEUR 67W | USB-C",
+    prix: "3.500 Fcfa",
+    // Description de 20 mots Max
+    description:"",
+    liens: "",
+  },
+  {
+    id: 11,
+    img: "./assets/USB-128GB.jpg",
+    nom: "CLE USB | 128GB",
+    prix: "8.000 Fcfa",
+    // Description de 20 mots Max
+    description:"",
+    liens: "",
+  },
+  {
+    id: 12,
+    img: "./assets/USB-64GB.jpg",
+    nom: "CLE USB | 64GB",
+    prix: "5.000 Fcfa",
+    // Description de 20 mots Max
+    description:"",
+    liens: "",
+  },
+  {
+    id: 13,
+    img: "./assets/USB-32GB.jpg",
+    nom: "CLE USB | 32GB",
+    prix: "4.000 Fcfa",
+    // Description de 20 mots Max
+    description:"",
+    liens: "",
+  },
+  {
+    id: 14,
+    img: "./assets/USB-16GB.jpg",
+    nom: "CLE USB | 16GB",
+    prix: "3.500 Fcfa",
+    // Description de 20 mots Max
+    description:"",
+    liens: "",
+  },
+    {
+    id: 17,
+    img: "./assets/USB-8GB.jpg",
+    nom: "CLE USB | 8GB",
+    prix: "3.000 Fcfa",
+    // Description de 20 mots Max
+    description:"",
+    liens: "",
+  },
+    {
+    id: 18,
+    img: "./assets/USB-4GB.jpg",
+    nom: "CLE USB | 4GB",
+    prix: "2.500 Fcfa",
+    // Description de 20 mots Max
+    description:"",
+    liens: "",
+  },
+   {
+    id: 19,
+    img: "./assets/USB-2GB.jpg",
+    nom: "CLE USB | 2GB",
+    prix: "2.000 Fcfa",
+    // Description de 20 mots Max
+    description:"",
+    liens: "",
+  },
+   {
+    id: 15,
+    img: "./assets/Memoire-64GB.jpg",
+    nom: "CARTE MEMOIRE | 64GB",
+    prix: "5.000 Fcfa",
+    // Description de 20 mots Max
+    description:"",
+    liens: "",
+  },
+  {
+    id: 16,
+    img: "./assets/Memoire-32GB.jpg",
+    nom: "CARTE MEMOIRE | 32GB",
+    prix: "4.000 Fcfa",
+    // Description de 20 mots Max
+    description:"",
+    liens: "",
+  },
+   {
+    id: 21,
+    img: "./assets/Pose-Téléphone.jpg",
+    nom: "Pose-Téléphone | Alu ",
+    prix: "2.000 Fcfa",
+    // Description de 20 mots Max
+    description:"",
+    liens: "",
+  },
+  {
+    id: 22,
+    img: "./assets/PB-10000mah.jpg",
+    nom: "Power Bank | 10.000mah ",
+    prix: "5.000 Fcfa",
+    // Description de 20 mots Max
+    description:"",
+    liens: "",
+  },
+  {
+    id: 23,
+    img: "./assets/PBL-20000mah.jpg",
+    nom: "Power Bank | 20.000mah ",
+    prix: "8.000 Fcfa",
+    // Description de 20 mots Max
+    description:"",
+    liens: "",
+  },
+  {
+    id: 24,
+    img: "./assets/PBL-30000mah.jpg",
+    nom: "Power Bank | 30.000mah ",
+    prix: "11.000 Fcfa",
+    // Description de 20 mots Max
+    description:"",
+    liens: "",
+  },
+   {
+    id: 25,
+    img: "./assets/Cable-C-iphone.jpg",
+    nom: "CABLE - C-IPHONE ",
+    prix: "1.500 Fcfa",
+    // Description de 20 mots Max
+    description:"",
+    liens: "",
+  },
+  {
+    id: 27,
+    img: "./assets/Cable-USB-C.jpg",
+    nom: "CABLE-USB-C ",
+    prix: "1.500 Fcfa",
+    // Description de 20 mots Max
+    description:"",
+    liens: "",
+  },
+  {
+    id: 26,
+    img: "./assets/Cable-iphone.jpg",
+    nom: "CABLE-USB-IPHONE ",
+    prix: "1.500 Fcfa",
+    // Description de 20 mots Max
+    description:"",
+    liens: "",
+  },
+  {
+    id: 28,
+    img: "./assets/Cable-USB--C.jpg",
+    nom: "CABLE-USB-C ",
+    prix: "1.500 Fcfa",
+    // Description de 20 mots Max
+    description:"",
+    liens: "",
+  },
+  {
+    id: 29,
+    img: "./assets/Cable-USB-V8.jpg",
+    nom: "CABLE-USB-V8 ",
+    prix: "1.500 Fcfa",
+    // Description de 20 mots Max
+    description:"",
+    liens: "",
+  },
+  {
+    id: 30,
+    img: "./assets/Chargeur-USB-IPHONE.jpg",
+    nom: "CHARGEUR-USB-IPHONE",
+    prix: "2.500 Fcfa",
+    // Description de 20 mots Max
+    description:"",
+    liens: "",
+  },
+  {
+    id: 31,
+    img: "./assets/Chargeur-USB-C.jpg",
+    nom: "CHARGEUR-USB-C",
+    prix: "2.500 Fcfa",
+    // Description de 20 mots Max
+    description:"",
+    liens: "",
+  },
+  {
+    id: 32,
+    img: "./assets/Chargeur-V8.jpg",
+    nom: "CHARGEUR-V8",
+    prix: "2.500 Fcfa",
+    // Description de 20 mots Max
+    description:"",
+    liens: "",
+  },
+  {
+    id: 33,
+    img: "./assets/Chargeur-C-Iphone.jpg",
+    nom: "CHARGEUR-C-IPHONE",
+    prix: "2.500 Fcfa",
+    // Description de 20 mots Max
+    description:"",
+    liens: "",
+  },
+  
+];
+// Dernier numero id:33
